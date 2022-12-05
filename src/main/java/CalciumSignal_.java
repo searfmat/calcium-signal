be71a5c72b218f81dfe2a5161de01f4470f70dc7
Matthew Searfass <searfmat@udel.edu>
2022-12-05 22:45:31 +0000

    Update src/main/java/CalciumSignal_.java, src/main/java/custom_roiManager.java, README.md, release/CalciumSignal Installer.py Deleted pythonscript/histogramscript.py, release/histogramscript.py

--- src/main/java/CalciumSignal_.java
+++ src/main/java/CalciumSignal_.java
@@ -3,26 +3,13 @@ import ij.*;
 import ij.gui.*;
 import ij.measure.ResultsTable;
 import ij.plugin.PlugIn;
-import ij.plugin.filter.Analyzer;
-import ij.plugin.frame.RoiManager;
-import ij.process.ColorProcessor;
-import ij.process.ImageProcessor;
-import ij.text.TextPanel;
-import ij.text.TextWindow;
 import imageJ.plugins.PoorMan3DReg_;
 
 import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.awt.image.ImageObserver;
-import java.awt.image.ImageProducer;
-import java.io.*;
-import java.util.Arrays;
-import java.util.Scanner;
 import java.io.IOException;
 
 public class CalciumSignal_ implements PlugIn {
     private final String EDGE_DATA_PATH = "plugins/CalciumSignal/edge_data";
-    private final String PYTHONSCRIPT_PATH = "plugins/CalciumSignal/pythonscript";
 
     public void run(String arg) {
 
@@ -49,9 +36,7 @@ public class CalciumSignal_ implements PlugIn {
 
             IJ.run("Z Project...", "projection=[Max Intensity]");
             img.close();
-            IJ.run(  "Enhance Contrast", "saturated=4 normalize");
-
-
+            IJ.run("Enhance Contrast", "saturated=4 normalize");
 
             counter.run(arg);
         }
@@ -74,51 +59,39 @@ public class CalciumSignal_ implements PlugIn {
         WindowManager.toFront(roiWindow);
 
         runRoiManager();
-
-
     }
     void runRoiManager(){
 
         //Creates RoiManager
         custom_roiManager crm = new custom_roiManager();
 
-        //Creates New scanner of edgeDetection CSV
-        try {
-            String pathName = EDGE_DATA_PATH + "/edgeDetectResults.csv";
-            Scanner scan = new Scanner(new File(pathName));
+        //Vars
+        double x;
+        double y;
+        double width;
+        double height;
+        int cornerDiameter = 20;
 
-            //Vars
-            double x;
-            double y;
-            double width;
-            double height;
-            String line;
-            String splitLine[];
-            int cornerDiameter = 20;
+        ResultsTable results = ResultsTable.getResultsTable();
 
-            //Skips first line
-            scan.nextLine();
+        double[] widths = results.getColumn("B-width");
+        double[] heights = results.getColumn("B-height");
+        double[] xs = results.getColumn("X");
+        double[] ys = results.getColumn("Y");
 
-            while (scan.hasNext()) {
-                line = scan.nextLine();
-                splitLine = line.split("[,]");
+        for(int i = 0; i < widths.length; i++) {
 
-                // This is used to make sure we have x and y at the center of the detected region
-                width = Double.parseDouble(splitLine[24]);
-                height = Double.parseDouble(splitLine[25]);
-                x = Double.parseDouble(splitLine[12]) - width/2 ;
-                y = Double.parseDouble(splitLine[13]) - height/2;
+            // This is used to make sure we have x and y at the center of the detected region
+            width = widths[i];
+            height = heights[i];
+            x = xs[i] - width/2 ;
+            y = ys[i] - height/2;
 
-                //Create ROI with Input: int x, int y, int width, int height, int cornerDiameter
-                Roi roi = new Roi((int)x, (int)y, (int)width, (int)height, cornerDiameter);
+            //Create ROI with Input: int x, int y, int width, int height, int cornerDiameter
+            Roi roi = new Roi((int)x, (int)y, (int)width, (int)height, cornerDiameter);
 
-
-                //Add Roi to RoiManager
-                crm.addRoi(roi);
-
-            }
-        }catch(IOException e) {
-            e.printStackTrace();
+            //Add Roi to RoiManager
+            crm.addRoi(roi);
         }
 
     }
@@ -128,3 +101,4 @@ public class CalciumSignal_ implements PlugIn {
         // System.out.println("Hello, world!");
     }
 }
+
